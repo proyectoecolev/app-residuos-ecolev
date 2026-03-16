d4ac8556df9eb6f2cfca6bdce764a040489cafe9
proyectoecolev <proyectoecolev@gmail.com>
2026-03-16 03:07:40 -0300

    Update marcar-retirado.js

--- pages/api/marcar-retirado.js
+++ pages/api/marcar-retirado.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
       body: JSON.stringify({
         socio_id,
         fecha,
-        estado: "retirado",
+        estado: "recolectado",
       }),
     });
 
